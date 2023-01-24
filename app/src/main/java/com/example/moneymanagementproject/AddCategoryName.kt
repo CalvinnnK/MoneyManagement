68e1dd155fddc62e2a22835f55b776e965fc9f15
tesaieo <mangkangtesalonika@gmail.com>
2023-01-24 12:45:46 +0700

    commit req

--- app/src/main/java/com/example/moneymanagementproject/AddCategoryName.kt
+++ app/src/main/java/com/example/moneymanagementproject/AddCategoryName.kt
@@ -13,6 +13,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
+
 class AddCategoryName : DialogFragment() {
     private var _binding : FragmentAddCategoryNameBinding? = null
     private val binding get() = _binding!!
